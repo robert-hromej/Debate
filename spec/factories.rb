a4f9b046d3d2dc77c2df3dee586dd6832687132f
Tom Koptel <tom.koptel@gmail.com>
2011-09-13 15:44:40 +0300

    factories v.0.2

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,18 @@
 require 'factory_girl'
 require 'faker'
+id_user = lambda{User.first.try(:id) || Factory(:user).id}
 
 FactoryGirl.define do
  factory :user do
-    sequence(:oauth_login) { |n| "oauth-login#{n}"}
-    sequence(:oauth_secret) { |n| "oauth-secret#{n}"}
+    sequence(:oauth_secret) { |n| "oauth-login#{n}"}
+    sequence(:oauth_token) { |n| "oauth-secret#{n}"}
     sequence(:screen_name) { |n| "screen-name#{n}"}
  end
 
  factory :debate_vote do
-    user
-    dquestion_ass
-    sequence(:current_vote) { |n| Kernel::Rand(1000)}
+    user_id { id_user }
+    debate_question
+    sequence(:current_vote) { |n| Kernel::rand(1000)}
  end
 
   factory :dquestion_ass, :class => DebateQuestion do
@@ -20,13 +21,13 @@ FactoryGirl.define do
 
   factory :debate_question do
     sequence(:body) {Faker::Lorem.sentence(2)}
-    user
+    user_id { id_user }
    end
 
   factory :comment do
-    user_id { User.first.try(:id) || Factory(:user).id }
+    user_id { id_user }
     sequence(:body) {Faker::Lorem.sentence(2)}
-    debate_question_id {DebateQuestion.first.try(:id) || Factory(:debate_vote).id}
+    debate_question_id { DebateQuestion.first.try(:id) || Factory(:debate_vote).id }
   end
 
 end
