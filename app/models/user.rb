ce1801b440ada4f5b365c6477b90e309e1bfe6c9
Fedir Kovbel <kfedia@gmail.com>
2011-09-13 14:46:40 +0300

    user update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,31 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :comment_votes
 
+  validates :screen_name, :uniqueness => true, :presence => true, :length => {:maximum => 255}
+  validates :oauth_secret, :presence => true
+  validates :oauth_token, :presence => true
+
+  # generate twitter's url for getting profile image using screen name
+  def profile_url
+    "http://twitter.com/#{self.screen_name}"
+  end
+
+  # searches user using screen name or create new one with specific oauth token and secret
+  def self.login(credentials, result)
+    # use exiting record for already registered users
+    u = User.where(:screen_name => credentials.screen_name).first
+    # Creates record in table user for new user
+    u ||= User.create(:screen_name => credentials.screen_name, :oauth_token => result.token, :oauth_secret => result.secret)
+  end
+
+  # create complete Twitter::Client object for working with twitter under specific user account
+  def client
+    options = {
+        :consumer_key => APP_CONFIG[:twitter][:consumer_token],
+        :consumer_secret => APP_CONFIG[:twitter][:consumer_secret],
+        :oauth_token => self.oauth_token,
+        :oauth_token_secret => self.oauth_secret
+    }
+    Twitter::Client.new(options)
+  end
 end
