4425d849681f36798940da8b577ffce3cc596625
Tom Koptel <tom.koptel@gmail.com>
2011-09-13 15:53:18 +0300

    factories v.0.4

--- spec/models/debate_vote_spec.rb
+++ spec/models/debate_vote_spec.rb
@@ -6,10 +6,14 @@ describe DebateVote do
   it{should respond_to(:user)}
 
   describe "validations" do
-    it "should not saved 'without user'"
+    it "should not saved 'without user'" do
+      #DebateVote.new(:debate_question)
+    end
     it "should not saved 'without debate_question'"
     context "should accept [-1,0,1]" do
-
+      it "Try 1"
+      it "Try 0"
+      it "Try -1"
     end
   end
 end
