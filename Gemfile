1bd40f0fd74f922cbdd3de3448b37c9dc9e725c4
Robert Hromej <robert.hromej@gmail.com>
2011-09-13 13:12:00 +0300

    Firts commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+
+#gem 'sqlite3'
+gem 'mysql2'
+
+group :development, :test do
+  gem "rspec", "~> 2.6.0"
+  gem "rspec-rails", "~> 2.4"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debate::Application.config.secret_token = '32b29b12dee478927dd6a06915c1ab48f7642177e330a5df7c8c4878a81e37d1e1f65b73b34fe85fe256d1deae8be0a4fb610b1732b2ce8217d2eea565b99757'
