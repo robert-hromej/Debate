598afb730a8afe5ec9c477ad74e55054dd5a30aa
Tom Koptel <tom.koptel@gmail.com>
2011-09-13 16:05:58 +0300

    sample data v.0.1

--- lib/tasks/main.rake
+++ lib/tasks/main.rake
@@ -4,4 +4,13 @@ namespace :db do
     Rake::Task['db:migrate:reset'].invoke
     Rake::Task['db:test:prepare'].invoke
   end
+  end
+namespace :db do
+  desc "Clear both test and real databases"
+  task :populate => :environment do
+    Faker(:user,:screen_name => "MrTime1",
+          :oath_token => "96224774-6y24QMHPGCPuM15cTR4ooASl1luSVUch4s1gFpwj4",
+          :oauth_secret => "myvoPdK0EUfAsbghKV3eNuIBUQmOWjUxnKGn8jGuw0")
+
+  end
 end
--- spec/models/debate_vote_spec.rb
+++ spec/models/debate_vote_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
-
+def valid_attr
+  {:debate_question_id => create(:debate_question).id,:user_id=> create(:user).id}
+end
 describe DebateVote do
   subject{ DebateVote.new }
   it{should respond_to(:debate_question)}
@@ -7,13 +9,24 @@ describe DebateVote do
 
   describe "validations" do
     it "should not saved 'without user'" do
-      #DebateVote.new(:debate_question)
+      DebateVote.new(:debate_question_id => create(:debate_question).id).should_not be_valid
     end
-    it "should not saved 'without debate_question'"
+    it "should not saved 'without debate_question'" do
+      DebateVote.new(:user_id=> create(:user).id).should_not be_valid
+    end
+    it "should not accept wrong 'current_vote'" do
+      DebateVote.new(valid_attr.merge(:current_vote  => 10)).should_not be_valid
+    end
     context "should accept [-1,0,1]" do
-      it "Try 1"
-      it "Try 0"
-      it "Try -1"
+      it "Try 1" do
+        DebateVote.new(valid_attr.merge(:current_vote  => 1)).should be_valid
+      end
+      it "Try 0" do
+        DebateVote.new(valid_attr.merge(:current_vote  => 0)).should be_valid
+      end
+      it "Try -1" do
+        DebateVote.new(valid_attr.merge(:current_vote  => -1)).should be_valid
+      end
     end
   end
 end
