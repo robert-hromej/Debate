f2d5b17d10a251362c5c135aaf26a4faf5f40d12
Fedir Kovbel <kfedia@gmail.com>
2011-09-15 13:42:03 +0300

    comment creating error handling

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,9 @@ class CommentsController < ApplicationController
 
   def create
     debate_question = DebateQuestion.find(params[:debate_question_id])
-    @comment = debate_question.comments.create(params[:comment].merge(:user_id => current_user.id))
-
-    push_error_message @comment.errors
-
-    redirect_to debate_question
+    @comment = debate_question.comments.create!(params[:comment].merge(:user_id => current_user.id))
+  rescue StandardError => ex
+    push_error_message ex.record.errors
   end
 
 end
