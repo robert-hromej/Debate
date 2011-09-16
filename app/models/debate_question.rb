cbc12f8b7f2309e1e2703d75c178338545b4cec3
Robert Hromej <robert.hromej@gmail.com>
2011-09-16 18:35:39 +0300

    bugfixing analytic chart in debate question show page

--- app/models/debate_question.rb
+++ app/models/debate_question.rb
@@ -31,24 +31,7 @@ class DebateQuestion < ActiveRecord::Base
   def uniq_votes
     DebateVote.uniq_votes.where(:debate_question_id => self.id)
   end
-  
-  def analytics_data
-    analytics_data_hash = {
-      :Yes => DebateQuestion.analytics_vote_count(self.id, 1),
-      :No => DebateQuestion.analytics_vote_count(self.id, -1),
-      :Neutral =>  DebateQuestion.analytics_vote_count(self.id, 0),
-    }
 
-    analytics_data_hash
-  end
-
-  def recounting
-    result = DebateVote.sub_query(uniq_votes).count(:id, :group => :current_vote)
-    self.update_attributes(:yes_count => result[DebateVote::YES] || 0,
-                           :no_count => result[DebateVote::NO] || 0,
-                           :neutral_count => result[DebateVote::NEUTRAL] || 0)
-  end
-  
   def vote?(user_id)
     if user_vote(user_id)
       @user_vote.vote
@@ -61,13 +44,30 @@ class DebateQuestion < ActiveRecord::Base
     @user_vote ||= debate_votes.where(:user_id => user_id).last
   end
 
+  def analytics_data
+    analytics_data_hash = {
+        :Yes => analytics_vote_count(1),
+        :No => analytics_vote_count(-1),
+        :Neutral => analytics_vote_count(0),
+    }
+
+    analytics_data_hash
+  end
+
+  def recounting
+    result = DebateVote.sub_query(uniq_votes).count(:id, :group => :current_vote)
+    self.update_attributes(:yes_count => result[DebateVote::YES] || 0,
+                           :no_count => result[DebateVote::NO] || 0,
+                           :neutral_count => result[DebateVote::NEUTRAL] || 0)
+  end
+
   private
 
-  def self.analytics_vote_count(debate_id, vote)
+  def analytics_vote_count(vote)
     data_array = []
-    select("UNIX_TIMESTAMP(DATE(debate_votes.updated_at))*1000 as date, count(debate_votes.current_vote) as count_by_day").
-    joins(:debate_votes).where("debate_votes.debate_question_id = ? AND debate_votes.current_vote = ?", debate_id, vote).
-    group("DATE(debate_votes.updated_at)").each do |data|
+
+    DebateVote.sub_query(uniq_votes).select("UNIX_TIMESTAMP(DATE(debate_votes.created_at))*1000 as date, count(debate_votes.current_vote) as count_by_day").
+        where(:current_vote => vote).group("DATE(debate_votes.created_at)").each do |data|
       count_array = []
       count_array << data.date.to_i
       count_array << data.count_by_day.to_i
