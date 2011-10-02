86fbc460d892e33f4d7e86970d31f27c79dc577e
EdgarK <kedgark@gmail.com>
2011-10-02 20:45:04 +0300

    comment_vote model test

--- spec/models/comment_vote_spec.rb
+++ spec/models/comment_vote_spec.rb
@@ -4,7 +4,7 @@ describe CommentVote do
   before(:all) do
     @attr = {:user_id => 1, :comment_id => 1}
   end
-  subject { CommentVote.new }
+
   it { should respond_to(:user_id) }
   it { should respond_to(:user) }
   it { should respond_to(:comment_id) }
@@ -16,6 +16,14 @@ describe CommentVote do
 
   it "should not be valid without comments" do
     CommentVote.new(@attr.merge(:comment_id => nil)).should_not be_valid
+  end
+
+  it "should should triger recounting after save" do
+    comment_vote = Factory(:comment_vote, :comment_id => Factory(:comment).id, :user_id => Factory(:user).id)
+    comment = stub
+    comment_vote.stub(:comment => comment)
+    comment.should_receive(:recounting).and_return(true)
+    comment_vote.update_attribute(:comment_id, Factory(:comment).id)
   end
 
 end
